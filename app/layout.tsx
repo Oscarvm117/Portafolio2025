c567996ecf13ebcea40295b4c5e67efcf253c7db
Oscarvm117 <oscarvemo@unisabana.edu.co>
2025-08-12 19:46:01 -0500

    dock

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,33 @@
 import LetterGlitch from "./components/LetterGlitch";
-
+import Dock from "./components/Dock";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  // Define the items array for the Dock componen t
+ // Replace with your actual items
+
+
+const items = [
+  { 
+    icon: <Image src="/yo.svj" alt="Yo" width={32} height={32} />, 
+    label: "Me", 
+    onClick: () => alert("") 
+  },
+  { 
+    icon: <Image src="/skills.svj" alt="Skills" width={32} height={32} />, 
+    label: "Skills", 
+    onClick: () => alert("") 
+  },
+  { 
+    icon: <Image src="/projects.svj" alt="Projects" width={32} height={32} />, 
+    label: "Projects", 
+    onClick: () => alert("") 
+  },
+]
+
   return (
     <html lang="es">
       <body>
@@ -15,6 +38,15 @@ export default function RootLayout({
           outerVignette={false}
           smooth={true}
         />
+
+        {/*DOCK */}
+        <Dock 
+          items={items}
+          panelHeight={68}
+          baseItemSize={50}
+          magnification={70}
+        />
+
         
         {/* Tu contenido */}
         <div className="relative z-10">
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-// Solo el contenido específico de tu página principal
 export default function HomePage() {
   return (
     <div className="min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-white">Mi Portafolio</h1>
+      <h1 className="text-4xl font-bold text-white">Oscar Vergara </h1>
       {/* Resto del contenido de tu página principal */}
     </div>
   );
